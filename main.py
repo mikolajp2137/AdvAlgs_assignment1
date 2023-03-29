b6e1ec4bf48ca156e0313d686967161633b9fe0b
Mikolaj <pienkoszmikolaj@gmail.com>
2023-03-29 17:38:33 +0200

    Rabin-Karp

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import Sunday
+import RabinKarp
 
 with open('raven.txt', encoding='utf8') as f:
     poem = f.read().replace('\n', ' ')
 
-
 pattern = "Raven"
 
-print(Sunday.sundaySearch(pattern,poem))
+print(Sunday.sundaySearch(pattern, poem))
+print(RabinKarp.rabinKarp(pattern, poem))
