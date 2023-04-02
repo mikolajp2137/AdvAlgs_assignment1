b8c39f836bc8bf0184d1ba8c453bb699c0c1b084
Mikolaj <pienkoszmikolaj@gmail.com>
2023-04-03 01:41:00 +0200

    added comments to SundayWildcards.py with explanations about how Sunday's algorithm works and general explanations of code there. Now code in that file fully supports ? and * wildcards and also allows them to be treated normally thanks to \. Few changes still to be made.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from Algorithms import RabinKarp, Sunday, SundayWildcards
 with open('poison.txt', encoding='utf8') as f:
     poem = f.read().replace('\n', ' ')
 
-patternWild = "wr*h"
-pattern = "wrath"
+patternWild = "I was angry"
+pattern = "I was angry"
 
 wildSundayResults = SundayWildcards.wildSundaySearch(patternWild, poem)
 print(wildSundayResults)
