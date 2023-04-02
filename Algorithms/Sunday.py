c7e67a3d14f0f72876329082a99e42960d932e73
Mikolaj <pienkoszmikolaj@gmail.com>
2023-04-03 01:50:33 +0200

    Unified naming and overall structure of Sunday.py and SundayWildcards.py so it's not necessary to add explanations in both files

--- Algorithms/Sunday.py
+++ Algorithms/Sunday.py
@@ -1,18 +1,21 @@
-def sundaySearch(pattern: str, d: str) -> int:
-    m, n = len(pattern), len(d)
-    occurrence = {}
-    for j in range(m):
-        occurrence[pattern[j]] = j
+def sundaySearch(pattern, text):
+    matches = []
+    m = len(pattern)
+    n = len(text)
+    skip_table = {}
+    for i in range(m):
+        skip_table[pattern[i]] = m - i
     i = 0
-    result = []
     while i <= n - m:
-        if pattern == d[i:i + m]:
-            result.append(i)
-            i += 1
+        j = 0
+        while j < m and (text[i + j] == pattern[j] or pattern[j] == "?"):
+            j += 1
+        if j == m:
+            matches.append(i)
+        if i + m >= n:
+            break
+        if text[i + m] in skip_table:
+            i += skip_table[text[i + m]]
         else:
-            next_char = d[i + m] if i + m < n else None
-            if next_char in occurrence:
-                i += m - occurrence[next_char]
-            else:
-                i += m - occurrence.get(d[i + m - 1], -1)
-    return result
+            i += m + 1
+    return matches
