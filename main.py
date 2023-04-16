2065d30ae494b40ca29c2af14ca46e3f28a85864
Mikolaj <pienkoszmikolaj@gmail.com>
2023-04-16 20:50:08 +0200

    Sunday now works with wildcards as required by the professor and also notes on how the algorithm works are now in a separate file

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from Algorithms import RabinKarp, Sunday, SundayWildcards, WildNaive
 with open('poison.txt', encoding='utf8') as f:
     poem = f.read().replace('\n', ' ')
 
-patternWild = "I*s a?g*y"
-#pattern = "And I"
+patternWild = "I w*s angry"
+pattern = "And I"
 
-# wildSundayResults = SundayWildcards.wildSundaySearch(patternWild, poem)
-# print("Sunday with wildcards: ")
-# print(wildSundayResults)
-#
+wildSundayResults = SundayWildcards.wildSundaySearch(patternWild, poem)
+print("Sunday with wildcards: ")
+print(wildSundayResults)
+## print("Naive/Bruteforce: ")
+# print(WildNaive.brute_force(poem, patternWild))
+
 # print("Sunday: ")
 # print(Sunday.sundaySearch(pattern, poem))
 #
 # print("Rabin-Karp: ")
 # print(RabinKarp.rabinKarp(pattern, poem))
 
-print("Naive/Bruteforce: ")
-print(WildNaive.brute_force(poem, patternWild))
