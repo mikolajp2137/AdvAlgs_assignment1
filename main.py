249864add270a334d258b7edd0daf1d932144d80
Mikolaj <pienkoszmikolaj@gmail.com>
2023-04-28 17:11:55 +0200

    for bruteforce: \ works for both, for sunday: \ works for *

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from Algorithms import RabinKarp, Sunday, SundayWildcards, WildNaive
 with open('poison.txt', encoding='utf8') as f:
     poem = f.read().replace('\n', ' ')
 
-patternWild = "I w*s angry"
+patternWild = "I w\?s angry"
 pattern = "And I"
 
 wildSundayResults = SundayWildcards.wildSundaySearch(patternWild, poem)
 print("Sunday with wildcards: ")
 print(wildSundayResults)
-## print("Naive/Bruteforce: ")
-# print(WildNaive.brute_force(poem, patternWild))
+print("Naive/Bruteforce: ")
+print(WildNaive.brute_force(poem, patternWild))
 
 # print("Sunday: ")
 # print(Sunday.sundaySearch(pattern, poem))
