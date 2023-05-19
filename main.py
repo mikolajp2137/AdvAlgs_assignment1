c15d393d9fa12eadd54ef2cc71561ef107c57c66
Mikolaj <pienkoszmikolaj@gmail.com>
2023-05-19 15:49:00 +0200

    done

--- main.py
+++ main.py
@@ -3,18 +3,10 @@ from Algorithms import RabinKarp, Sunday, SundayWildcards, WildNaive
 with open('poison.txt', encoding='utf8') as f:
     poem = f.read().replace('\n', ' ')
 
-patternWild = "I w\?s angry"
+patternWild = "I ?as a\*"
 pattern = "And I"
 
-wildSundayResults = SundayWildcards.wildSundaySearch(patternWild, poem)
 print("Sunday with wildcards: ")
-print(wildSundayResults)
+print(SundayWildcards.wildSundaySearch(patternWild, poem))
 print("Naive/Bruteforce: ")
-print(WildNaive.brute_force(poem, patternWild))
-
-# print("Sunday: ")
-# print(Sunday.sundaySearch(pattern, poem))
-#
-# print("Rabin-Karp: ")
-# print(RabinKarp.rabinKarp(pattern, poem))
-
+print(WildNaive.brute_force(patternWild, poem))
